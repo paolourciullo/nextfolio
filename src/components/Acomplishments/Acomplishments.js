ad4b9f79bd85593f59df7c469177e57c8458efeb
Paolo U <paolourciullo@gmail.com>
2021-07-23 16:01:38 -0400

    change accomp text

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -8,10 +8,9 @@ import {
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles'
 
 const data = [
-  { number: 20, text: 'Open Source Projects' },
-  { number: 1000, text: 'Students' },
-  { number: 1900, text: 'Github Followers' },
-  { number: 5000, text: 'Github Stars' },
+  { number: 7, text: 'Open Source Projects' },
+  { number: 52, text: 'Github Followers' },
+  { number: 14, text: 'Github Stars' },
 ]
 
 const Acomplishments = () => (
